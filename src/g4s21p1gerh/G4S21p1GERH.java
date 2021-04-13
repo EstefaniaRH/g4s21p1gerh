95cd4a81583680c388869d533072e3271d4888b7
estef <estef@UmaruRH>
2021-04-12 21:49:56 -0500

    Primer código en NetBeans

---
+++ src/g4s21p1gerh/G4S21p1GERH.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g4s21p1gerh;
+
+/**
+ *
+ * @author estef
+ */
+public class G4S21p1GERH {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmPrincipal frmprincipal = new frmPrincipal();
+        frmprincipal.setVisible(true);
+        
+    
+    } 
+    
+    
+}
